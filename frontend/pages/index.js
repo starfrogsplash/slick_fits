0bcca78635de3810a8d79f8e36e0b8476d10dd2f
Ali Khalil <ali_khalil@hotmail.co.uk>
2019-08-24 10:32:19 +0100

    added in next basic routing

---
+++ frontend/pages/index.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const index = () => (
+  <div>
+    HI
+    <p>
+      <Link href="/sell">
+        <a> Sell!</a>
+      </Link>
+    </p>
+  </div>
+);
+
+export default index;
---
+++ frontend/pages/sell.js
@@ -0,0 +1,20 @@
+import Link from "next/link";
+ 
+ const Sell = () => {
+        return (
+            <div>
+                Sell
+                <p>
+                <Link href="/">
+                  <a> Home!</a>
+                </Link>
+              </p>
+            </div>
+        )
+}
+
+
+export default Sell
+
+
+
