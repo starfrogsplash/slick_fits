73ac092c3923621ef3cf13b5e32c16906f5b5047
Ali Khalil <ali_khalil@hotmail.co.uk>
2019-08-24 20:58:43 +0100

    add in routing

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,6 +1,12 @@
 import Link from 'next/link';
 import NavStyles from './styles/NavStyles';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 
+Router.onRouteChangeStart = () => NProgress.start()
+Router.onRouteChangeComplete = () =>  NProgress.done();
+Router.onRouteChangeError = () =>   NProgress.done();
+ 
  const Nav = () => (
   <NavStyles>
        <Link href="/items">
